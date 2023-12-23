6575cf4ffa5998956af5f168e8629f8bb34e5d0e
ehab-abdulmejid <ehab.abdulmejid@unity3d.com>
2023-12-22 19:24:29 -0800

    update code

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -35,7 +35,7 @@ public class NewTestScript
         sb.Append("</body>");
         sb.Append("</html>");
         
-        StreamWriter sw1 = new StreamWriter(@"TestResults/Doc.html");
+        StreamWriter sw1 = new StreamWriter(@"docs/index.html");
         
         sw1.Write(sb);
         sw1.Flush();
